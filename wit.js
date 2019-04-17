711001973ff334e8b017cc279fb45c3a19652038
Tom Simmons <tomsimmons@Toms-MacBook-Pro.local>
2019-04-17 11:48:12 +0100

    create getIntent model

---
+++ wit.js
@@ -0,0 +1,12 @@
+const {Wit} = require('node-wit')
+const {accessToken} = require('./config.js')
+
+const client = new Wit({
+    accessToken
+})
+
+const getIntent = (text)=>{
+    return client.message(text)
+}
+
+module.exports = getIntent;
